66ac784a794b42b95a949ffc349af7c4a23b9c5a
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-27 01:57:44 +0900

    Create p180.py

---
+++ p180.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+array = []
+for _ in range(N):
+  input_data = input().split()
+
+  array.append((input_data[0], int(input_data[1])))
+
+array = sorted(array, key=lambda student: student[1])
+
+for student in array:
+  print(student[0], end=' ')
