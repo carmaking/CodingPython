78a64cd8eb31eec1d85a1d2eaf48323e5992e8b8
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-29 14:14:35 +0900

    Create p311.py

---
+++ p311.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+data = list(map(int, input().split()))
+data.sort()
+count = 0
+result = 0
+temp = data[0]
+
+for i in data:
+  
+  count += 1
+  if count >= i:
+    result += 1
+    count = 0
+
+print(result)
